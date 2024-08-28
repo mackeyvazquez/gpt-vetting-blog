bba9918b1e5cd168843f5c34efa970c065f1e71a
adriana ruiz <adriannaruiz24@gmail.com>
2024-08-28 11:28:24 -0500

    style: fix page style

--- packages/blog-starter-kit/themes/enterprise/components/navbar.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/navbar.tsx
@@ -3,9 +3,9 @@ import { Search } from './searchbar';
 
 export const Navbar = () => {
 	return (
-		<div className="mt-20 w-full flex flex-col justify-center items-center space-y-1 gap-2 text-left sm:space-y-0 md:flex-row md:max-w-lg md:mx-auto">
+		<div className="pt-28 w-full flex flex-col justify-center items-center gap-2 md:max-w-lg md:mx-auto">
 			<Search />
-			<Button type="primary" label="Search" className='w-full flex justify-center md:w-auto !py-2' />
+			<Button type="primary" label="Search" className='w-full md:text-sm flex justify-center md:py-2 md:px-4' />
 		</div>
 	);
 };
--- packages/blog-starter-kit/themes/enterprise/components/searchbar.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/searchbar.tsx
@@ -104,14 +104,14 @@ export const Search = () => {
 	});
 
 	return (
-		<div className="relative col-span-1">
+		<div className="relative col-span-1 w-full">
 			<input
 				type="text"
 				ref={searchInputRef}
 				onKeyUp={escapeSearchOnESC}
 				onChange={updateSearchQuery}
 				placeholder="Search blog posts…"
-				className="w-full rounded-full border border-slate-200 bg-slate-50 px-4 py-2 text-base focus:bg-transparent dark:border-neutral-800 dark:bg-neutral-800 dark:text-neutral-50 dark:placeholder:text-neutral-400 dark:hover:bg-neutral-950"
+				className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
 			/>
 			{query && (
 				<>

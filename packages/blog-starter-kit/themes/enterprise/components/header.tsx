ff5f9722112e5fa89ea7321d11d8c86f796f247c
Mackey Vazquez <mackeyvazquez@gmail.com>
2024-09-01 15:05:21 +0100

    Fixed header links

--- packages/blog-starter-kit/themes/enterprise/components/header.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/header.tsx
@@ -16,29 +16,34 @@ export const Header = () => {
 	const title = publication.title;
 
 	return (
-		<header className="h-14 w-full px-4 bg-white fixed z-30 flex items-center border-b dark:border-neutral-300 dark:bg-neutral-300 lg:px-6">
-			
+		<header className="fixed z-30 flex h-14 w-full items-center border-b bg-white px-4 dark:border-neutral-300 dark:bg-neutral-300 lg:px-6">
 			<Container className="w-fulls flex justify-between gap-1">
-					<div className="flex items-center justify-center transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground">
-						<Button 
-							href='/@plicableai' 
-							as='a' 
-							type='outline'
-							icon={
-								<img
-									alt={publication.title}
-									src={logo ?? ''}
-									className='h-6 w-6'
-								/>
-							}
-							label={title ?? ''}
-							className='md:py-1 md:space-x-2 !text-xl'
-						/>
-					</div>
+				<div className="focus-visible:ring-ring hover:bg-accent hover:text-accent-foreground flex items-center justify-center transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50">
+					<Button
+						href="/"
+						as="a"
+						type="outline"
+						icon={<img alt={publication.title} src={logo ?? ''} className="h-6 w-6" />}
+						label={title ?? ''}
+						className="!text-xl md:space-x-2 md:py-1"
+					/>
+				</div>
 
 				<nav className="col-span-2 flex flex-row items-center justify-end gap-3 lg:col-span-3">
-					<Button href='/@plicableai' as="a" type="outline" label="Demo" className='!font-medium hover:underline hover:bg-transparent md:text-sm'/>
-					<Button href='https://www.gptvetting.com/login' as="a" type="outline" label="Login" className='!font-medium !bg-slate-100 md:text-sm'/>
+					<Button
+						href="/@aplicableai"
+						as="a"
+						type="outline"
+						label="Demo"
+						className="!font-medium hover:bg-transparent hover:underline md:text-sm"
+					/>
+					<Button
+						href="https://www.gptvetting.com/login"
+						as="a"
+						type="outline"
+						label="Login"
+						className="!bg-slate-100 !font-medium md:text-sm"
+					/>
 				</nav>
 			</Container>
 		</header>

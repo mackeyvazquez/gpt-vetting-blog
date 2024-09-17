1273a6f9422e0c85ea7688774de43423b2eb0a15
Mackey Vazquez <mackeyvazquez@gmail.com>
2024-09-17 15:43:47 +0100

    Fixed https error footer

--- packages/blog-starter-kit/themes/enterprise/components/footer.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/footer.tsx
@@ -1,11 +1,9 @@
-
 export const Footer = () => {
-
 	return (
 		<>
 			<iframe
-				src='http://gptvetting.com/embed/footer'
-				className='w-full h-28 sm:h-20 md:h-14'
+				src="https://gptvetting.com/embed/footer"
+				className="h-28 w-full sm:h-20 md:h-14"
 			></iframe>
 		</>
 	);
